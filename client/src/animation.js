a2426a9194ecc68312d1280d70e6f1df50069168
Elfric <elf72742@gmail.com>
2022-06-28 17:32:03 -0400

    Setting up input parameters

--- client/src/animation.js
+++ client/src/animation.js
@@ -1,1 +1,31 @@
 import Konva from 'konva';
+
+const animate = {
+  anim: null
+};
+
+animate.create = ({angle, distance}) => {
+  // calculate duration etc from input data
+
+  animate.anim = new Konva.Animation((node) => {
+
+  }, layer);
+};
+
+animate.reset = () => {
+  animate.anim = null;
+};
+
+animate.play = () => {
+  if (animate.anim !== null) {
+    animate.anim.start();
+  }
+};
+
+animate.stop = () => {
+  if (animate.anim !== null) {
+    animate.anim.stop();
+  }
+};
+
+export default animate;
--- client/src/main.js
+++ client/src/main.js
@@ -7,13 +7,53 @@ import animate from './animation.js';
 // $('#input-angle')
 // $('#input-distance')
 
+// Dance parameters
+const params = {
+  angle: 0,
+  distance: 200
+};
+
+// Static DOM Elements
+$('#input-distance').prop({
+  step: 200,
+  min: 200,
+  max: 5000,
+});
+
+$('#input-angle').prop({
+  value: 0,
+  min: 0,
+  max: 359
+});
+
+// Events
 const startStopAnimation = function(e) {
   if (e.target.innerText === 'Play') {
     e.target.innerText = 'Stop';
+    $('.input-params').prop('disabled', true);
+
+    animate.create(params);
+    animate.play();
   } else {
     e.target.innerText = 'Play';
+    $('.input-params').prop('disabled', false);
+    animate.stop();
   }
 };
 
-// Click events
+
 $('#btn-play').on('click', startStopAnimation);
+
+$('#input-angle').on('change', () => {
+  let angle = $('#input-angle').prop('value');
+  if (angle === '' || isNaN(Number(angle))) {
+    $('#input-angle').prop('value', params.angle);
+  } else {
+    params.angle = $('#input-angle').prop('value');
+  }
+});
+
+$('#input-distance').on('change', () => {
+  params.distance = $('#input-distance').prop('value');
+});
+
